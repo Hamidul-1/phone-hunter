4644375e9c56c23acf73ab714957eed4fd333c14
Hamidul-1 <hamidul786cse@gmail.com>
2022-03-01 20:58:18 +0600

    first commit

--- phone.js
+++ phone.js
@@ -1,10 +1,13 @@
-
 const allPhones = () => {
-    const searchPhone = document.getElementById('search-phone').value;
-    // searchPhone.value = '';
+    
+    const searchField = document.getElementById('search-phone');
+    const searchPhone = searchField.value;
+    // clear data
+    searchField.value = '';
+
+    // load data
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`;
     fetch(url)
-    // console.log(url);
     .then(response => response.json())
     .then(data => showMobileDetails(data.data));
 };
@@ -12,7 +15,12 @@ const allPhones = () => {
 const showMobileDetails = mobiles => {
     // console.log(mobiles);
     const searchMobile = document.getElementById('search-mobile');
-    mobiles.forEach(mobile => {
+    if (!mobiles[0]){
+      document.getElementById('hide2').style.display = 'block'
+    }
+    else{
+      document.getElementById('hide2').style.display = 'none'
+      mobiles.forEach(mobile => {
         console.log(mobile);
         const div = document.createElement('div');
         div.classList.add('col');
@@ -31,6 +39,27 @@ const showMobileDetails = mobiles => {
         `;
         searchMobile.appendChild(div);
     })
+    }
+    
+    /* mobiles.forEach(mobile => {
+        console.log(mobile);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${mobile.image}" class="card-img-top w-50" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Phone Name: ${mobile.phone_name}</h5>
+              <h5>Phone Brand: ${mobile.brand}</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="all-button">
+                <button onclick="details('${mobile.slug}')" class="btn btn-success">Details</button>
+            </div>
+            </div>
+          </div>
+        `;
+        searchMobile.appendChild(div);
+    }) */
     
 };
 
@@ -46,14 +75,31 @@ const details = (id) => {
 const setDetails = (info) => {
   console.log(info);
   const phoneDetails = document.getElementById('phone-details');
+  phoneDetails.textContent = '';
   const div = document.createElement('div');
   div.classList.add('card');
   div.innerHTML = `
-  <img src="${info.mainFeatures.image}" class="card-img-top" alt="...">
+  <img src="${info.image}" class="card-img-top w-50 mx-auto" alt="...">
   <div class="card-body">
-    <h5 class="card-title">${info.mainFeatures}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Name: ${info.name}</h5>
+
+    <h5>Brand: ${info.brand}</h5>
+    <h5><b>Release Date</b></h5>
+    <h6>${info.releaseDate ? info.releaseDate : 'No-release date found' }</h6>
+    <h5><b>Main Features</b></h5>
+    <h6>Chip Set: ${info.mainFeatures.chipSet}</h6>
+    <h6>Display Size: ${info.mainFeatures.displaySize}</h6>
+    <h6>Memory: ${info.mainFeatures.memory}</h6>
+    <h6>Storage: ${info.mainFeatures.storage}</h6>
+    <h6>Sensor: ${info.mainFeatures.sensors}</h6>
+    <h5><b>Others</b></h5>
+    <h6>WLAN: ${info.others ? info.others.WLAN : 'No WLAN found'}</h6> 
+    <h6>Bluetooth: ${info.others ? info.others.Bluetooth :'No'}</h6>
+    <h6>GPS: ${info.others ? info.others.GPS : 'No'}</h6>
+    <h6>NFC: ${info.others ? info.others.NFC  :'No'}</h6>
+    <h6>Radio: ${info.others ?info.others.Radio : 'No'}</h6>
+
+    
   </div>
   `;
   phoneDetails.appendChild(div);
